4591e0f9d24d39d4e0b657cb0c3139424aaccf09
Rhema Great <84960398+greatrhema@users.noreply.github.com>
2024-08-17 22:25:17 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+// Even or Odd Function
+
+function compare(number) {
+    if (number % 2 === 0) {
+        return "Even";
+    } else {
+        return "Odd";
+    }
+}
+console.log(compare(4)); 
+
+
+// Add Item to List
+
